84a83a64d08a08aa6b5c242d7974c1c7e6cc10b5
OsbornChen <osbornc@nycstudents.net>
2025-10-09 11:44:41 -0400

    Second commit, Main.java and WordMatct.java are blue

--- src/WordMatch.java
+++ src/WordMatch.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class WordMatch {
-}
+    private String secret;
+
+    public int scoreGuess(String guess) {
+
+        return 0;
+    }
+
+    public WordMatch(String word){
+
+    }
+
+    public String findBetterGuess(String guess1, String guess2){
+        if (scoreGuess("guess1") > scoreGuess("guess2") ){
+            return "guess1";
+        } else if (scoreGuess("guess1") < scoreGuess("guess2")
+        return "guess2";
+        }
+    }
+
