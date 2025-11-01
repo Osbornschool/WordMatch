9822810f8343e4f524f72a66d8192d7003aa272b
Osbornschool <Osbornc@nycstudents.net>
2025-10-31 20:21:19 -0400

    14646 WORKING

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
 import java.io.FileNotFoundException;
 import java.io.File;
-import java.nio.channels.WritePendingException;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
-        System.out.println(read());
+        System.out.println("Total Score: " + read());
     }
 //        WordMatch w = new WordMatch("mississippi");
 //        System.out.println(w.scoreGuess("i"));
@@ -22,22 +21,21 @@ public class Main {
 //        System.out.println(game.scoreGuess("cat"));
 //        System.out.println(game.findBetterGuess("con", "cat"));
 
-    public static String read() throws FileNotFoundException {
+    public static int read() throws FileNotFoundException {
         File f = new File("Guesses.txt");
         Scanner s = new Scanner(f);
-        int index = 0;
         int counter = 0;
         while (s.hasNext()) {
-            String a = "placeholder1";
-            String b = "placeholder2";
-            String c = "placeholder3";
-            a = (s.next());
-            b = (s.next());
-            c = (s.next());
+            String a = (s.next());
+            String b = (s.next());
+            String c = (s.next());
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(c);
             WordMatch w = new WordMatch(a);
-            System.out.println(w.findBetterGuessValue(a, c));
-            
+            System.out.println(w.findBetterGuessValue(b, c));
+            counter += (w.findBetterGuessValue(b, c));
         }
-        return "WIP";
+        return counter;
     }
 }
